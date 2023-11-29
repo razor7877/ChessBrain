511d02f5365feb14c17049d7addf457e6f8ad1b2
razor7877 <razor7877@gmail.com>
2023-11-29 14:07:25 -0500

    Made board row-major for easier manipulation, added helper getSpot() method, added method to dump board content to console

--- include/game/board.hpp
+++ include/game/board.hpp
@@ -4,12 +4,19 @@
 
 class Spot;
 
+// A class representing a board object, which contains an 8x8 matrix of spots for the chess pieces
 class Board
 {
 public:
+	// A two dimensional array containing the spots
+	// It's a row major array, the first index selects the row, the second one select the column
 	Spot** boxes;
+
 	Board();
 	~Board();
+	// Resets the board to it's starting state
 	void resetBoard();
+	void dumpBoardToConsole();
+	Spot getSpot(int x, int y);
 };
 #endif
--- src/game/board.cpp
+++ src/game/board.cpp
@@ -18,64 +18,6 @@ Board::Board()
 	this->resetBoard();
 }
 
-void Board::resetBoard()
-{
-	// Black pieces
-	this->boxes[0][7] = Spot(&Rook(false), 1, 8);
-	this->boxes[1][7] = Spot(&Knight(false), 2, 8);
-	this->boxes[2][7] = Spot(&Bishop(false), 3, 8);
-	this->boxes[3][7] = Spot(&Queen(false), 4, 8);
-	this->boxes[4][7] = Spot(&King(false), 5, 8);
-	this->boxes[5][7] = Spot(&Bishop(false), 6, 8);
-	this->boxes[6][7] = Spot(&Knight(false), 7, 8);
-	this->boxes[7][7] = Spot(&Rook(false), 8, 8);
-	this->boxes[0][7] = Spot(&Pawn(false), 1, 7);
-	this->boxes[1][6] = Spot(&Pawn(false), 2, 7);
-	this->boxes[2][6] = Spot(&Pawn(false), 3, 7);
-	this->boxes[3][6] = Spot(&Pawn(false), 4, 7);
-	this->boxes[4][6] = Spot(&Pawn(false), 5, 7);
-	this->boxes[5][6] = Spot(&Pawn(false), 6, 7);
-	this->boxes[6][6] = Spot(&Pawn(false), 7, 7);
-	this->boxes[7][6] = Spot(&Pawn(false), 8, 7);
-
-	// White pieces
-	this->boxes[0][1] = Spot(&Rook(true), 1, 1);
-	this->boxes[1][1] = Spot(&Knight(true), 2, 1);
-	this->boxes[2][1] = Spot(&Bishop(true), 3, 1);
-	this->boxes[3][1] = Spot(&Queen(true), 4, 1);
-	this->boxes[4][1] = Spot(&King(true), 5, 1);
-	this->boxes[5][1] = Spot(&Bishop(true), 6, 1);
-	this->boxes[6][1] = Spot(&Knight(true), 7, 1);
-	this->boxes[7][1] = Spot(&Rook(true), 8, 1);
-	this->boxes[0][0] = Spot(&Pawn(true), 1, 2);
-	this->boxes[1][0] = Spot(&Pawn(true), 2, 2);
-	this->boxes[2][0] = Spot(&Pawn(true), 3, 2);
-	this->boxes[3][0] = Spot(&Pawn(true), 4, 2);
-	this->boxes[4][0] = Spot(&Pawn(true), 5, 2);
-	this->boxes[5][0] = Spot(&Pawn(true), 6, 2);
-	this->boxes[6][0] = Spot(&Pawn(true), 7, 2);
-	this->boxes[7][0] = Spot(&Pawn(true), 8, 2);
-
-	for (int x = 0; x < 8; x++)
-	{
-		for (int y = 0; y < 8; y++)
-		{
-			if (&this->boxes[x][y] != NULL)
-			{
-				std::string type = "unset";
-				switch (this->boxes[x][y].piece->getType())
-				{
-				case PieceType::PAWN:
-					type = "Pawn";
-				default:
-					type = "Default";
-				}
-				printf("Piece at (%d,%d): %s\n", x, type.c_str());
-			}
-		}
-	}
-}
-
 Board::~Board()
 {
 	for (int i = 0; i < 8; i++)
@@ -83,4 +25,99 @@ Board::~Board()
 		delete this->boxes[i];
 	}
 	delete this->boxes;
+}
+
+void Board::resetBoard()
+{
+	// Black pieces
+	this->boxes[7][0] = Spot(&Rook(false), 1, 8);
+	this->boxes[7][1] = Spot(&Knight(false), 2, 8);
+	this->boxes[7][2] = Spot(&Bishop(false), 3, 8);
+	this->boxes[7][3] = Spot(&Queen(false), 4, 8);
+	this->boxes[7][4] = Spot(&King(false), 5, 8);
+	this->boxes[7][5] = Spot(&Bishop(false), 6, 8);
+	this->boxes[7][6] = Spot(&Knight(false), 7, 8);
+	this->boxes[7][7] = Spot(&Rook(false), 8, 8);
+	this->boxes[6][0] = Spot(&Pawn(false), 1, 7);
+	this->boxes[6][1] = Spot(&Pawn(false), 2, 7);
+	this->boxes[6][2] = Spot(&Pawn(false), 3, 7);
+	this->boxes[6][3] = Spot(&Pawn(false), 4, 7);
+	this->boxes[6][4] = Spot(&Pawn(false), 5, 7);
+	this->boxes[6][5] = Spot(&Pawn(false), 6, 7);
+	this->boxes[6][6] = Spot(&Pawn(false), 7, 7);
+	this->boxes[6][7] = Spot(&Pawn(false), 8, 7);
+
+	// White pieces
+	this->boxes[1][0] = Spot(&Rook(true), 1, 1);
+	this->boxes[1][1] = Spot(&Knight(true), 2, 1);
+	this->boxes[1][2] = Spot(&Bishop(true), 3, 1);
+	this->boxes[1][3] = Spot(&Queen(true), 4, 1);
+	this->boxes[1][4] = Spot(&King(true), 5, 1);
+	this->boxes[1][5] = Spot(&Bishop(true), 6, 1);
+	this->boxes[1][6] = Spot(&Knight(true), 7, 1);
+	this->boxes[1][7] = Spot(&Rook(true), 8, 1);
+	this->boxes[0][0] = Spot(&Pawn(true), 1, 2);
+	this->boxes[0][1] = Spot(&Pawn(true), 2, 2);
+	this->boxes[0][2] = Spot(&Pawn(true), 3, 2);
+	this->boxes[0][3] = Spot(&Pawn(true), 4, 2);
+	this->boxes[0][4] = Spot(&Pawn(true), 5, 2);
+	this->boxes[0][5] = Spot(&Pawn(true), 6, 2);
+	this->boxes[0][6] = Spot(&Pawn(true), 7, 2);
+	this->boxes[0][7] = Spot(&Pawn(true), 8, 2);
+
+	// Fill up the remaining board with empty spots
+	for (int x = 2; x < 6; x++)
+	{
+		for (int y = 0; y < 8; y++)
+		{
+			this->boxes[x][y] = Spot(nullptr, x + 1, y + 1);
+		}
+	}
+
+	this->dumpBoardToConsole();
+}
+
+void Board::dumpBoardToConsole()
+{
+	for (int x = 0; x < 8; x++)
+	{
+		for (int y = 0; y < 8; y++)
+		{
+			Spot spot = this->boxes[x][y];
+			std::string type = "Empty";
+			if (spot.piece != nullptr)
+			{
+				switch (spot.piece->getType())
+				{
+				case PieceType::PAWN:
+					type = "Pawn";
+					break;
+				case PieceType::BISHOP:
+					type = "Bishop";
+					break;
+				case PieceType::KING:
+					type = "King";
+					break;
+				case PieceType::QUEEN:
+					type = "Queen";
+					break;
+				case PieceType::KNIGHT:
+					type = "Knight";
+					break;
+				case PieceType::ROOK:
+					type = "Rook";
+					break;
+				default:
+					type = "Error";
+					break;
+				}
+			}
+			printf("Spot at (%d,%d): adr %x name %s\n", spot.x, spot.y, (int)spot.piece, type.c_str());
+		}
+	}
+}
+
+Spot Board::getSpot(int x, int y)
+{
+	return this->boxes[y][x];
 }
